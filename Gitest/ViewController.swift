ebe704c0110c5f1041e5169179fc51d855ac9151
SURYA KUMAR SUNGER <suryakumarsunger@SURYAs-iMac.local>
2018-01-26 20:21:11 +0530

    background color is changed to red

---
+++ Gitest/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Gitest
+//
+//  Created by SURYA KUMAR SUNGER on 26/01/18.
+//  Copyright © 2018 Antispol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
